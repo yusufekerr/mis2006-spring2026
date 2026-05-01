6f62acc41ca057509e01206e639f8b4cfbec7091
Yusuf Eker <138672649+yusufekerr@users.noreply.github.com>
2026-05-01 15:26:38 +0000

    app.jsx file edited because of the crashs.

--- mis2006-campus-portal/src/App.jsx
+++ mis2006-campus-portal/src/App.jsx
@@ -7,74 +7,6 @@ import Courses from "./pages/Courses";
 import Register from "./pages/Register";
 import ApiDemo from "./pages/ApiDemo";
 
-export default function App() {
-   return ( 
-    <main> 
-      <h1>MIS2006 Campus Course Portal</h1> 
-        <p>React lab project running in GitHub Codespaces.</p> 
-    </main> 
-  );
-}
-
-
-export default function App() {
-   return ( 
-    <main> 
-      <h1>MIS2006 Campus Course Portal</h1> 
-      
-      <CourseCard 
-        code="MIS2006" 
-        title="Web Programming" 
-        instructor="Asst. Prof. Dr. Ergun Eray AKKAYA" 
-        credits={3} 
-        isOpen={true} 
-      /> 
-    </main> 
-  ); 
-}
-
-export default function App() {
-  return (
-    <main>
-      <h1>MIS2006 Campus Course Portal</h1>
-        <CourseBadgeClass code="MIS2006" level="Undergraduate" />
-    </main> 
-  );
-}
-
-export default function App() { 
-  return ( 
-  <main> 
-    <h1>MIS2006 Campus Course Portal</h1> 
-    <EnrollmentCounter /> 
-  </main> 
-  );
-}
-
-export default function App() { 
-  return ( 
-  <div className="app-shell"> 
-    <header> 
-      <h1>MIS2006 Campus Course Portal</h1> 
-      <nav> 
-        <Link to="/">Home</Link> 
-        <Link to="/courses">Courses</Link> 
-        <Link to="/register">Register</Link> 
-      </nav> 
-    </header> 
-    
-    <main> 
-      <Routes> 
-        <Route path="/" element={<Home />} /> 
-        <Route path="/courses" element={<Courses />} /> 
-        <Route path="/register" element={<Register />} /> 
-        <Route path="*" element={<p>404 - Page not found</p>} /> 
-      </Routes> 
-    </main> 
-  </div> 
-  );
-}
-
 export default function App() { 
   return ( 
     <div className="app-shell"> 
@@ -99,4 +31,4 @@ export default function App() {
       </main> 
     </div> 
   );
-}
+} 
